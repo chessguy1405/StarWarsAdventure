2ac5a60cd1b100c4240d2672fb7df71d6029fb30
Jeremy Nielsen <51862520+chessguy1405@users.noreply.github.com>
2020-04-23 13:58:14 -0700

    Started over, began using pygame zero. Just music

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
+import pgzrun
 import pygame
 pygame.init()
-
-
 
 #consants
 WIDTH = 1400
 HEIGHT = 800
 TITLE = "A Star Wars Adventure"
-BACKGROUND_IMAGE = 'images/space-background.png'
-#Background music for the majority of the game
-pygame.mixer.music.load('music/main_theme.mp3')
-pygame.mixer.music.play(0)
 
-#background level 1
-pygame.image.load('images/space-background.png')
-background_image = pygame.image.load('images/space-background.png')
-size = (WIDTH, HEIGHT)
-screen = pygame.display.set_mode(size)
-screen.blit(background_image, [0, 0])
+#other variables
 
-novaclass = pygame.image.load('images/nova-ship.png')
-
-class GameObject:
-    def __init__(self, image, height, speed):
-        self.speed = speed
-        self.image = image
-        #self.pos = image.get_rect(0, height)
-        self.draw = pygame.image.load(self.image)
-    #def move(self):
-        #self.pos = self.pos.move(0, self.speed)
-    #if self.pos.right > 600:
-    #    self.pos.left = 0
-novaship = GameObject('images/nova-ship.png', 80, 10)
-novaship.draw()
+#music
+pygame.music.mixer.load('music/main-theme')
+pygame.music.mixer.play(-1)
